a9896bdedbc4054dc0a77f487a050e8366d9e836
oleks <oleks@oleks.info>
2017-03-14 14:23:39 +0100

    Prettier indentation

--- binary16.h
+++ binary16.h
@@ -5,12 +5,12 @@
 
 typedef uint16_t binary16_t;
 
-binary16_t binary16_from_binary32(float x);
-binary16_t binary16_from_binary64(double x);
+binary16_t  binary16_from_binary32(float x);
+binary16_t  binary16_from_binary64(double x);
 
-float binary16_to_binary32(binary16_t);
-double binary16_to_binary64(binary16_t);
+float       binary16_to_binary32(binary16_t);
+double      binary16_to_binary64(binary16_t);
 
-binary16_t binary16_add(binary16_t x, binary16_t y);
+binary16_t  binary16_add(binary16_t x, binary16_t y);
 
 #endif // BINARY16
